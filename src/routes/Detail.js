af4c8c5d1d96ea50956f5796505b92ede07720e5
sonbaejun <sonqowns2@naver.com>
2023-10-31 17:57:46 +0900

    TIL : Styled-Components

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,20 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+/* let copyYBtn = styled.button(YellowBtn)`
+  margin-top: 10px
+`;
+ */
+let BlackBox = styled.div`
+  background: grey;
+  padding: 20px;
+`;
 
 function DetailPage(props) {
   let { id } = useParams();
@@ -7,7 +23,7 @@ function DetailPage(props) {
   return (
     <div className="container">
       <div className="row">
-        f
+        <BlackBox as="button"></BlackBox>
         <div className="col-md-6">
           <img
             src={`https://codingapple1.github.io/shop/shoes${imgId}.jpg`}
